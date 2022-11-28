7b8bab712566602b137f298ed10a00fc4fc22ee5
olsonjenna <jenna.forston@usu.edu>
2022-11-28 09:39:39 -0700

    Google Analytics
    added analytics

---
+++ js/overlaynavigation.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById('myNav').style.display = 'block';
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when user clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById('myNav').style.display = 'none';
+    document.getElementById("myNav").style.width = "0%";
+}
